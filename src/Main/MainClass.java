f5296f9ddc72b28f97be82f6987d8ee443877463
sameh <sameh@netservex.com>
2020-08-15 00:52:54 +0200

    second comment

---
+++ src/Main/MainClass.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i= 0 ; i < 20 ; i++) 
+		{
+			System.out.println("hello " + i);
+		}
+		
+		System.out.println("Done");
+	}
+
+}
